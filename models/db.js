931319624e810ec02f9dbd82129c82ac41c1341f
Carlo Aguirre <carlo_aguirre@outlook.cl>
2023-09-20 13:40:14 -0300

    ready for render.com

--- models/db.js
+++ models/db.js
@@ -1,12 +1,25 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
+// const pool = new Pool({
+//     connectionString: process.env.DB_URL,
+//     allowExitOnIdle: true
+//   });
+
 const pool = new Pool({
- host: 'localhost',
- user: 'postgres',
- password: '',  // RECUERDA PONER TU CLAVE DE POSTGRES
- database: 'marketplace',
+ host: 'dpg-cjvpkoh5mpss73adov4g-a.oregon-postgres.render.com',
+ user: 'carlo',
+ password: 'zHrtUcw4o5IlpcHM4nvb892XIsHZvkkg',  
+ database: 'marketplace_f0gh',
  allowExitOnIdle: true
 })
+
+// const pool = new Pool({
+//   host: 'localhost',
+//   user: 'postgres',
+//   password: '$Ony3434',
+//   database: 'marketplace',
+//   allowExitOnIdle: true
+//  })
 
 export{pool}
