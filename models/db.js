a95d43c9463dd4e826716eb4edb13a692baf006f
Carlo Aguirre <carlo_aguirre@outlook.cl>
2023-12-06 11:04:16 -0300

    db pool modified

--- models/db.js
+++ models/db.js
@@ -1,26 +1,13 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
-// const pool = new Pool({
-//     connectionString: process.env.DB_URL,
-//     allowExitOnIdle: true
-//   });
-
 const pool = new Pool({
- host: 'dpg-cjvpkoh5mpss73adov4g-a.oregon-postgres.render.com',
- user: 'carlo',
- password: 'zHrtUcw4o5IlpcHM4nvb892XIsHZvkkg',  
- database: 'marketplace',
+ host: 'ddpg-clo7ppkjtl8s73albicg-a.oregon-postgres.render.com',
+ user: 'marketplace_yxf3_user',
+ password: 'S4Lq9CvoWs7VS3RQQv3iKpmvYnMH0pCA',  
+ database: 'marketplace_yxf3',
  allowExitOnIdle: true,
  ssl: true,
   })
-
-// const pool = new Pool({
-//   host: 'localhost',
-//   user: 'postgres',
-//   password: '$Ony3434',
-//   database: 'marketplace',
-//   allowExitOnIdle: true
-//  })
 
 export{pool}
