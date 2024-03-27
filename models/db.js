1c70a069517e213c38e6bd12d1c94091be9117e6
Carlo Aguirre <carlo_aguirre@outlook.cl>
2024-03-27 12:01:14 -0300

    new pool

--- models/db.js
+++ models/db.js
@@ -2,10 +2,10 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
- host: 'ddpg-clo7ppkjtl8s73albicg-a.oregon-postgres.render.com',
- user: 'marketplace_yxf3_user',
- password: 'S4Lq9CvoWs7VS3RQQv3iKpmvYnMH0pCA',  
- database: 'marketplace_yxf3',
+ host: 'dpg-co237tg21fec73d4vbr0-a.oregon-postgres.render.com',
+ user: 'marketplace_t7f8_user',
+ password: 'oc4eejuAEOv6OYuNl42LE5eOXRfISFcE',  
+ database: 'marketplace_t7f8',
  allowExitOnIdle: true,
  ssl: true,
   })
